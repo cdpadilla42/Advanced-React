03834cb4d9c064c800949bbbe54b8547ad39a1e1
Chris Padilla <cdpadilla42@gmail.com>
2021-02-09 11:41:17 -0600

    Initialize pages

---
+++ sick-fits/frontend/components/Page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div>
+      <h2>I'm the page!</h2>
+    </div>
+  );
+};
+
+export default Page;
---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Page from '../components/Page';
+
+const Index = () => {
+  return <Page>Hello</Page>;
+};
+
+export default Index;
