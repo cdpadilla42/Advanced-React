221fede93ae72658722df0f96ae5256b963eb1ef
Chris Padilla <cdpadilla42@gmail.com>
2021-02-11 13:41:15 -0600

    Integrate keystone config

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Page = () => {
+const Page = ({ children, cool }) => {
   return (
     <div>
       <h2>I'm the page!</h2>
+      {children}
     </div>
   );
+};
+
+Page.propTypes = {
+  cool: PropTypes.string,
+  children: PropTypes.any,
 };
 
 export default Page;
