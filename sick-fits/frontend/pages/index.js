25a71597e9f1930fac2cab33d7a60604c56220db
Chris Padilla <cdpadilla42@gmail.com>
2021-02-19 10:55:34 -0600

    Add product page files

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,8 +1,1 @@
-import React from 'react';
-import Page from '../components/Page';
-
-const Index = () => {
-  return <div>Hello</div>;
-};
-
-export default Index;
+export { default } from './products';
