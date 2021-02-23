1488d55061c1dd27559e3f4770384414f9c02d43
Chris Padilla <cdpadilla42@gmail.com>
2021-02-23 13:13:31 -0600

    Style product display

--- sick-fits/frontend/components/SingleProduct.js
+++ sick-fits/frontend/components/SingleProduct.js
@@ -1,5 +1,21 @@
+import Head from 'next/head';
 import { gql, useQuery } from '@apollo/client';
 import DisplayError from './ErrorMessage';
+import styled from 'styled-components';
+
+const ProductStyles = styled.div`
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  max-width: var(--maxWidth);
+  justify-content: center;
+  align-items: top;
+  gap: 2rem;
+  img {
+    object-fit: contain;
+    width: 100%;
+  }
+`;
 
 const SINGLE_ITEM_QUERY = gql`
   query PRODUCT($id: ID!) {
@@ -20,12 +36,26 @@ const SINGLE_ITEM_QUERY = gql`
 export default function SingleProduct({ id }) {
   const { data, loading, error } = useQuery(SINGLE_ITEM_QUERY, {
     variables: {
-      id: id,
+      id,
     },
   });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <DisplayError error={error} />;
-  console.log({ data });
-  return <p>Hey singles {data.Product.name}</p>;
+  const { Product } = data;
+  return (
+    <ProductStyles>
+      <Head>
+        <title>Sick Fits | {Product.name}</title>
+      </Head>
+      <img
+        src={Product.photo.image.publicUrlTransformed}
+        alt={Product.photo.altText}
+      />
+      <div className="details">
+        <h2>{Product.name}</h2>
+        <p>{Product.description}</p>
+      </div>
+    </ProductStyles>
+  );
 }
