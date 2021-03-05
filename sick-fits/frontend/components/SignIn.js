29e8b51fc6b924bc7b653f89a6ba50808a7d1587
Chris Padilla <cdpadilla42@gmail.com>
2021-03-05 08:43:47 -0600

    Add error ui to sign in form

--- sick-fits/frontend/components/SignIn.js
+++ sick-fits/frontend/components/SignIn.js
@@ -1,6 +1,8 @@
 import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
+import { useState } from 'react';
 import useForm from '../lib/useForm';
+import DisplayError from './ErrorMessage';
 import Form from './styles/Form';
 
 const SIGN_IN_USER = gql`
@@ -29,34 +31,53 @@ export default function SignIn() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const result = await signInUser();
+    const { data: result } = await signInUser();
     console.log(result);
     // TODO render appropriate responses to success and failure
+    if (
+      result.authenticateUserWithPassword.__typename ===
+      'UserAuthenticationWithPasswordFailure'
+    ) {
+      console.log('FAILED');
+    }
+
+    if (result.authenticateUserWithPassword.sessionToken) {
+      console.log('Youre ine!');
+    }
   };
 
   return (
     <Form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email
-        <input
-          type="text"
-          name="email"
-          id="email"
-          value={inputs.name}
-          onChange={handleChange}
-        />
-      </label>
-      <label htmlFor="password">
-        Password
-        <input
-          type="password"
-          name="password"
-          id="password "
-          value={inputs.password}
-          onChange={handleChange}
-        />
-      </label>
-      <button type="submit">Sign In</button>
+      <DisplayError
+        error={
+          data?.authenticateUserWithPassword.code === 'FAILURE'
+            ? data?.authenticateUserWithPassword
+            : error
+        }
+      />
+      <fieldset disabled={loading} aria-busy={loading}>
+        <label htmlFor="email">
+          Email
+          <input
+            type="text"
+            name="email"
+            id="email"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            name="password"
+            id="password "
+            value={inputs.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Sign In</button>
+      </fieldset>
     </Form>
   );
 }
