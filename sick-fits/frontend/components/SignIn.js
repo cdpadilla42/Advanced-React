7503588b8d741f07bf789db9576f83169157d0bc
Chris Padilla <cdpadilla42@gmail.com>
2021-03-05 09:15:06 -0600

    Redirect on successful log in

--- sick-fits/frontend/components/SignIn.js
+++ sick-fits/frontend/components/SignIn.js
@@ -1,9 +1,10 @@
+import { useRouter } from 'next/router';
 import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
-import { useState } from 'react';
 import useForm from '../lib/useForm';
 import DisplayError from './ErrorMessage';
 import Form from './styles/Form';
+import { CURRENT_USER_QUERY } from './User';
 
 const SIGN_IN_USER = gql`
   mutation SIGN_IN_USER($email: String!, $password: String!) {
@@ -20,41 +21,34 @@ const SIGN_IN_USER = gql`
 `;
 
 export default function SignIn() {
-  const { inputs, handleChange } = useForm({
+  const router = useRouter();
+  const { inputs, handleChange, clearForm } = useForm({
     email: '',
     password: '',
   });
 
   const [signInUser, { data, loading, error }] = useMutation(SIGN_IN_USER, {
     variables: inputs,
+    // refetch current logged in user
+    refetchQueries: [{ query: CURRENT_USER_QUERY }],
   });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const { data: result } = await signInUser();
-    console.log(result);
     // TODO render appropriate responses to success and failure
-    if (
-      result.authenticateUserWithPassword.__typename ===
-      'UserAuthenticationWithPasswordFailure'
-    ) {
-      console.log('FAILED');
-    }
-
     if (result.authenticateUserWithPassword.sessionToken) {
       console.log('Youre ine!');
+      clearForm();
+      // Push to home page
+      router.push('/');
     }
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <DisplayError
-        error={
-          data?.authenticateUserWithPassword.code === 'FAILURE'
-            ? data?.authenticateUserWithPassword
-            : error
-        }
-      />
+    <Form method="POST" onSubmit={handleSubmit}>
+      <h2>Sign Into Your Account</h2>
+      <DisplayError error={data?.authenticateUserWithPassword || error} />
       <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="email">
           Email
@@ -62,6 +56,8 @@ export default function SignIn() {
             type="text"
             name="email"
             id="email"
+            placeholder="Email Address"
+            autoComplete="email"
             value={inputs.name}
             onChange={handleChange}
           />
@@ -71,7 +67,8 @@ export default function SignIn() {
           <input
             type="password"
             name="password"
-            id="password "
+            id="password"
+            placeholder="password"
             value={inputs.password}
             onChange={handleChange}
           />
